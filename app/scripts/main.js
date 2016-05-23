03c2d1231a7527a97303c52a34d98babf4f02a84
Faisal Jeaumar <faisal.jeaumar@gmail.com>
2016-05-23 10:36:41 +0700

    Basic scaffolding
    - Add i18next plugin for multilingual

---
+++ app/scripts/main.js
@@ -0,0 +1,22 @@
+// console.log('\'Allo \'Allo!');
+
+
+
+// $(document).ready(function(){
+//    i18next.init({
+//     lng: 'en',
+//     resources: {
+//         en: {
+//         translation: {
+//             "key": "hello world"
+//         }
+//         }
+//     }
+//     }, (err, t) => {
+//     // initialized and ready to go!
+//     const hw = i18next.t('key'); // hw = 'hello world'
+//     console.log(hw);
+//     $('#spanId').text(hw);
+//     });
+// });
+
